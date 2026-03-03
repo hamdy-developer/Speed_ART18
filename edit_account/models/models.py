63efb149e34de227dbcff6247796c666a0f03574
Developer <developer@smart-home.local>
2026-03-03 19:11:09 +0200

    edit_account -

--- edit_account/models/models.py
+++ edit_account/models/models.py
@@ -30,10 +30,10 @@ class AccountMove(models.Model):
 
     def action_post(self):
         for move in self:
-            if move.move_type == 'out_invoice' and move.partner_id.credit_limit > 0:
+            if move.move_type == 'out_invoice' and move.partner_id.sudo().credit_limit > 0:
                 # The 'credit' field on res.partner is a computed field that calculates the total credit of the partner.
                 # It is the standard way to get the partner's credit in Odoo.
-                allowed_credit = move.partner_id.credit_limit - move.partner_id.credit
+                allowed_credit = move.partner_id.sudo().credit_limit - move.partner_id.sudo().credit
                 if move.amount_total > allowed_credit:
                     raise UserError(_('Cannot validate invoice. Credit limit exceeded for partner: %s') % move.partner_id.name)
         return super(AccountMove, self).action_post()
@@ -82,9 +82,9 @@ class SaleOrder(models.Model):
 
     def action_confirm(self):
         for order in self:
-            if order.partner_id.credit_limit > 0:
+            if order.partner_id.sudo().credit_limit > 0:
 
-                allowed_credit = order.partner_id.credit_limit - order.partner_id.credit
+                allowed_credit = order.partner_id.sudo().credit_limit - order.partner_id.sudo().credit
                 if order.amount_total > allowed_credit:
                     raise UserError(_('Cannot confirm sale order. Credit limit exceeded for partner: %s') % order.partner_id.name)
         return super(SaleOrder, self).action_confirm()
