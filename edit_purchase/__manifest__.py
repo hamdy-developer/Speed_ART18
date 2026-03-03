15fcae97c1f64a2761588e991ae29b0a971cd3ed
Developer <developer@smart-home.local>
2026-03-03 14:10:55 +0200

    edit_purchase - invoice Print Report

--- edit_purchase/__manifest__.py
+++ edit_purchase/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'category': 'Purchase',
     'summary': 'Customizations for Purchase module',
-    'depends': ['purchase', 'stock', 'product'],
+    'depends': ['purchase', 'stock', 'product', 'purchase_stock'],
     'data': [
         'views/product_tag_views.xml',
         'views/purchase_order_views.xml',
--- edit_purchase/models/__init__.py
+++ edit_purchase/models/__init__.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
-
-from . import product_tag
-from . import purchase_order
 from . import purchase_order_line
+from . import product_tag
